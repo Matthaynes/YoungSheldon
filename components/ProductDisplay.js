1d72005ddebc9677f765e1e4357b7cfcea3bfe49
Matthew Haynes <124mhaynes@frhsd.com>
2024-03-14 09:05:26 -0400

    put pictures in and got it working

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -56,10 +56,12 @@ app.component('product-display', {
           product: 'Shirts',
           brand: 'Walmart',
           selectedVariant: 0,
-          details: ['50% cotton', '30% wool', '20% polyester'],
+          details: ['20% cotton', '30% wool', '50% polyester'],
           variants: [
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            { id: 2234, color: 'red', image: './assets/images/red_shirt.png', quantity: 50 },
+            { id: 2235, color: 'blue', image: './assets/images/blue_shirt.png', quantity: 20 },
+            { id: 2236, color: 'gold', image: './assets/images/gold_shirt.png', quantity: 10 },
+            { id: 2237, color: 'white', image: './assets/images/white_shirt.png', quantity: 0 }
           ],
           reviews: []
       }
